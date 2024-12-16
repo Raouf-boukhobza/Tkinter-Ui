18cc69ddfc39a4a3d586f0a19348432b6262c650
raouf <raoufboukhobza3@gmail.com>
2024-12-16 20:50:45 +0100

    finish the furmulaire ui

--- furmulaire.py
+++ furmulaire.py
@@ -56,7 +56,7 @@ entry_type= ctk.StringVar()
 #display the image 
 
 inputs_frame = ctk.CTkFrame(window  , width=750 , fg_color="white" )
-create_lable(window=inputs_frame).grid(row = 0 , column =0 , columnspan =2, pady = (10 , 20) ,padx = (0 , 100) )
+create_lable(window=inputs_frame , text="Regestration Form" , image="images1.png").grid(row = 0 , column =0 , columnspan =2, pady = (10 , 20) ,padx = (0 , 100) )
 create_entry(window=inputs_frame , text="Nom : " , textVariable=entry_name).grid(row =1 , column = 0 , pady = 20 )
 create_entry(window=inputs_frame , text="Prénom : " , textVariable=entry_prenom).grid(row =2 , column = 0 , pady =20)
 create_entry(window=inputs_frame , text="N°Téléphone: " , textVariable=entry_numero_telephone).grid(row =3 , column = 0 , pady  =20 )
@@ -66,7 +66,7 @@ create_entry(window=inputs_frame , text="Date Naissance : " , textVariable=entry
 create_list(window=inputs_frame , text="Ville : ", textOption="choisie la ville", textvar=entry_ville, list_option=["jijel" , "oran" , "setif"]).grid(row = 3 , column = 1 , padx = (75 , 0) )
 create_entry(window=inputs_frame , text="Commune : " , textVariable=entry_commune).grid(row =4 , column = 1 , pady = 20, padx =(75 , 0))
 create_list(window=inputs_frame , text="Type : ",  textvar=entry_type ,textOption="choisie le type " , list_option=["f3" , "f2" , "f4"]).grid(row = 5 , column = 0 )
-button = ctk.CTkButton(master=inputs_frame , fg_color="#5D3FD3" , command= insert_data , text="Submit" , height=40,width=200)
+button = ctk.CTkButton(master=inputs_frame , fg_color="#5D3FD3" , command= insert_data , text="Submit" ,text_color="white", height=40,width=200)
 button.grid(row = 5 , column = 1 , padx = (75 , 0) , pady = (15 , 0))
 inputs_frame.pack( side='left', padx =100,pady = 50 , fill = 'both')
 
